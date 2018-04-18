36e2e423416f7b7063561a93c36567cc6fcdaa0c
SampsonM <SampsonM@github.com>
2018-04-18 18:16:03 +0100

    rmvd axios and refactored for component did mount, non funcitonal

--- nc-news/src/components/articles.js
+++ nc-news/src/components/articles.js
@@ -1,42 +1,70 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 import './css/articles.css'
 
+
+
 class Articles extends Component {
-  render () {
-    const param = this.props.match.params.sort;
-    const articles = getArticlesByParam(param)
-    return articles;
+  state = {
+    loading : true,
   }
-}
 
-function getArticlesByParam (param) {
-  return axios.get('https://northc-news.herokuapp.com/api/articles')
-    .then(res => {
-      const articleArr = res.data.slice(0);
-      return Promise.all([sortArticles(articleArr, param)]);
-    })
-    .then(([res]) => {
-      return console.log(res);
-    })
-}
-
-function sortArticles (articleArr, param) {
-    const sorted = articleArr.sort((a, b) => {
-      return b.votes - a.votes;
-    })
-
-
-    sorted.map(article => {
+  componentDidMount() {
+    const param = this.props.match.params.sort;
+    console.log(param)
+    this.getArticlesByParam(param);
+  }
+  
+  render () {
+    if (this.state.loading) {
+      return <div>Loading...</div>
+    }
+    if (!this.state.loading) {
       return (
-        <div class="card bg-light mb-3" style={{maxWidth: '100%'}}>
-          <div class="card-header">{article.title}</div>
-          <div class="card-body">
-            <p class="card-text">{article.body}</p>
-          </div>
+        <div>
+          {this.state.articles.map(article => {
+            return (
+              <div class="card bg-light mb-3" style={{maxWidth: '100%'}}>
+                <div class="card-header">{article.title}</div>
+                <div class="card-body">
+                  <p class="card-text">{article.body}</p>
+                </div>
+              </div>
+            )
+          })}
         </div>
       )
+    }
+  }
+
+
+
+  getArticlesByParam = (param) => {
+    this.setState({
+      loading : true
+     });
+  
+    return fetch('https://northc-news.herokuapp.com/api/articles')
+     .then(res => {
+        res.json();
+     })
+      .then(res => {
+        const articleArr = res.data.slice(0);
+        return Promise.all([this.sortArticles(articleArr, param)]);
+      })
+      .then(([res]) => {
+        this.setState({
+          articles : res,
+          loading: false
+        })
+        return res;
+      })
+  }
+
+  sortArticles = (articleArr, param) => {
+    return articleArr.sort((a, b) => {
+      return b.votes - a.votes;
     })
+  } 
 }
 
 
