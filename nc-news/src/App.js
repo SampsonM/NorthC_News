3c7620ba7076571e6958e98bcf70a61ece370293
SampsonM <SampsonM@github.com>
2018-04-18 16:03:17 +0100

    framework built for basic structure

--- nc-news/src/App.js
+++ nc-news/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Browser as Route, Link } from "react-router-dom";
+import Header from './components/header';
+import Articles from './components/articles';
+import Comments from './components/comments';
+import Topics from './components/topics';
+import Users from './components/users';
 import './components/css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <p>hello</p>
+        <Header />
+        <Route exact path="/" component={Articles} />
+        <Route path="/comments" component={Comments} />
+        <Route path="/Topics" component={Topics} />
+        <Route path="/Users" component={Users} />
       </div>
     );
   }
---
+++ nc-news/src/components/header.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import { Browser as Router, Route, Link } from "react-router-dom";
+import './css/header.css';
+
+class Header extends Component {
+  render () {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <Link class="navbar-brand" to="/">NC-News</Link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <Link class="nav-item nav-link" to="/">New<span class="sr-only"></span></Link>
+            <Link to="/" class="nav-item nav-link">Comments</Link>
+            <Link to="/" class="nav-item nav-link">Topics</Link>
+            <Link to="/" class="nav-item nav-link">Users</Link>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Header;
