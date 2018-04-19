163bfe7f848145740147b0d59207147f963391c4
SampsonM <SampsonM@github.com>
2018-04-19 16:50:00 +0100

    user function building

--- nc-news/src/components/users.js
+++ nc-news/src/components/users.js
@@ -1,16 +1,10 @@
 import React, {Component} from 'react';
+import {Route, Link} from 'react-router-dom';
 
 class Users extends Component {
   state = {
-    loading : true
-  }
-
-  componentDidMount() {
-    this.getUsers()
-  }
-
-  componentWillReceiveProps(nextProps) {
-    
+    loading : false,
+    submitted : false
   }
 
   render () {
@@ -18,27 +12,23 @@ class Users extends Component {
     if (!this.state.loading) {
       return (
         <div>
-          <p className="text-center card mt-4 mx-auto w-25">Search users!</p>
-          <div className="input-group mx-auto mt-5 w-50">
-            <div className="input-group-prepend">
-              <span onClick={() => {}} className="input-group-text" id="inputGroup-sizing-default">Search</span>
+          <div className="card text-white bg-danger pb-4">
+            <p className="text-center mt-4 mx-auto w-25">Search users!</p>
+            <div className="card-body input-group mx-auto w-50">
+              <Link to="/users/:user_id" user={this.state.inputValue} className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-default">Search</span>
+              </Link>
+              <input onChange={this.handleChange} type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
             </div>
-            <input onChange={this.handleChange} type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
           </div>
+          <br />
+          <hr />
+
+          <Route path="/users/:user_id"  component={User} />
         </div>
+
       )
     }
-  }
-
-  getUsers = () => {
-    fetch('https://northc-news.herokuapp.com/api/users')
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          users : res,
-          loading: false
-        })
-      })
   }
 
   handleChange = (event) => {
@@ -46,6 +36,44 @@ class Users extends Component {
     this.setState({
       inputValue : value
     })
+  }
+}
+
+// const User = props => {
+
+// }
+
+class User extends Component {
+  state = {
+    loading: true
+  }
+
+  componentDidMount() {
+    this.getUsers()
+  }
+
+  render() {
+    console.log(this.props.match.params)
+    if (this.state.loading) {
+      return (<div>LOADING...</div>)
+    }
+    if (!this.state.loading) {
+      return <div>ready!!</div>
+    }
+  }
+
+  getUsers = () => {
+    fetch('https://northc-news.herokuapp.com/api/users')
+      .then(res => res.json())
+      .then(res => {
+        this.getUserByName(res);
+      })
+  }
+
+  getUserByName = (users) => {
+    // users.filter(user => {
+    //   return user.name === 
+    // })
   }
 }
 
