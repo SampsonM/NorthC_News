7df12f8cb737396944cef04ad0e9d42d061cc36b
SampsonM <SampsonM@github.com>
2018-04-20 17:56:13 +0100

    loading screen

--- nc-news/src/components/Loading.js
+++ nc-news/src/components/Loading.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Loading extends Component {
   render () {
     return (
-      <div>LOADING...</div>
+      <div className="w-100 mx-auto">
+        <img className="mx-auto" src={`http://icon-park.com/imagefiles/loading7_red.gif`} style={{maxWidth: "50px"}}/>
+      </div>
     )
   }
 }
