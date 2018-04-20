cb60dea378dd8813a94c7b56debfc85779680a5c
SampsonM <SampsonM@github.com>
2018-04-20 17:47:10 +0100

    app functional profile page working

--- nc-news/src/components/Profile.js
+++ nc-news/src/components/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import {Loading} from './index';
 
 class Profile extends Component {
@@ -10,15 +11,8 @@ class Profile extends Component {
     const { username } = this.props.match.params;
     console.log(username)
     this.getUserInfo(username)
+    this.getUserArticles(username)
   }
-
-  // componentWillReceiveProps(nextprops) {
-  //   const { user } = nextprops.location.state;
-  //   console.log(nextprops.location)
-  //   this.setState({
-  //     user : user
-  //   })
-  // }
 
   render () {
     const { user } = this.state;
@@ -27,20 +21,30 @@ class Profile extends Component {
     }
     if (!this.state.loading) {
       return (
-        <div className="card mt-2 mx-auto" style={{width: "25rem"}}>
-          <img className="card-img-top" src={user.avatar_url} alt="profile face" />
-          <div className="card-body">
-            <h5 className="card-title">{user.name}</h5>
+        <div>
+          <div className="card mt-2 mx-auto" style={{width: "25rem"}}>
+            <img className="card-img-top" src={user.avatar_url} alt="profile face" />
+            <div className="card-body">
+              <h5 className="card-title">{user.name}</h5>
+            </div>
+            <ul className="list-group list-group-flush ">
+              <li className="list-group-item bg-danger text-white text-uppercase">{user.username}</li>
+            </ul>
           </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">{user.username}</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Vestibulum at eros</li>
-          </ul>
-          <div className="card-body">
-            <a className="card-link">Card link</a>
-            <a className="card-link">Another link</a>
+
+        <div>
+          <div class="list-group mx-5 mb-5">
+          <h5 className="bg-danger text-white mt-5 list-group-item">  {user.username}'s articles
+          </h5>
+            {this.state.articles.map(article => {
+              return (
+                <div>
+                  <Link to={`/articles/${article._id}`}><p className="list-group-item list-group-item-action">{article.title}</p></Link>
+                </div>
+              )
+            })}
           </div>
+        </div>
         </div>
       )
     }
@@ -52,8 +56,26 @@ class Profile extends Component {
       .then(res => {
         console.log(res)
         this.setState({
-          user: res[0],
-          loading: false
+          user: res[0]
+        })
+      })
+  }
+
+  getUserArticles = id => {
+    fetch(`https://northc-news.herokuapp.com/api/articles`)
+      .then(res => res.json())
+      .then(res => {
+        const userid = this.state.user._id;
+        return res.filter(article => {
+          if (article.created_by._id === userid) {
+            return article;
+          }
+        })
+      })
+      .then(res => {
+        this.setState({
+          articles : res,
+          loading : false
         })
       })
   }
